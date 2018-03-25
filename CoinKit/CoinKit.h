7a2b811f16549d46f323371950d17b36dfa2bde2
Elliott Minns <elliott.minns@me.com>
2018-03-25 12:49:50 -0500

    Initial Commit

---
+++ CoinKit/CoinKit.h
@@ -0,0 +1,19 @@
+//
+//  CoinKit.h
+//  CoinKit
+//
+//  Created by Elliott Minns on 25/03/2018.
+//  Copyright © 2018 Elliott Minns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoinKit.
+FOUNDATION_EXPORT double CoinKitVersionNumber;
+
+//! Project version string for CoinKit.
+FOUNDATION_EXPORT const unsigned char CoinKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoinKit/PublicHeader.h>
+
+
